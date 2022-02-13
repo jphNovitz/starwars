be12f8aa3de0d884806ed4428e2a6acc08107b7f
Novitz Jean-Philippe <novitz@gmail.com>
2022-02-13 03:55:50 +0100

    javascript refactor add readme file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # starwars
+
+Dans une video Benjamen code donne un défi  à quelques dev : réaliser une page web en deux heures
+j'ai réalisé ce petit travail sans regarder l'heure car j'ai travaillé par petits bout quand j'avais une poignée de
+temps disponible
+
+https://www.youtube.com/watch?v=Ld97MuYMaQQ
+
+# Liens
+
+- [Modèle Figma](https://www.figma.com/file/xaisLkNZJvZXq6Ca8qO6El/UI_StarWars?node-id=0%3A1)
+- [La chaine de Benjamen Code](https://www.youtube.com/c/BenjaminCode)
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,23 @@
+/**
+ *  @package Starwars
+ *  @author novitz Jean-Philippe <novitz@gmail.com>
+ *  @description Dans une video Benjamen code donne un défi  à quelques dev : réaliser une page web en deux heures
+ *  j'ai réalisé ce petit travail sans regarder l'heure car j'ai travaillé par petits bout quand j'avais une poignée de
+ *  temps disponible
+ */
+
 document.addEventListener('DOMContentLoaded', () => {
 
     const years = document.getElementById('years')
     const arrow_left = document.getElementById('arrow-left')
     const arrow_right = document.getElementById('arrow-right')
     const picture = document.getElementsByTagName('picture')[0]
-    var max_page = picture.getElementsByTagName('img').length
-    console.log(max_page)
-    var page = 1
-    var min_page = 1
-
-
     const pitch = document.getElementById('pitch')
     const bullets = document.getElementsByClassName('bullet')
-    console.log(bullets)
     const root = document.querySelector(":root");
-
+    const max_page = picture.getElementsByTagName('img').length
+    const min_page = 1
+    var page = 1
     const step_img = 24.51
     const step_pitch = 31.3
 
@@ -33,34 +36,41 @@ document.addEventListener('DOMContentLoaded', () => {
     function down(e) {
         e.preventDefault()
         if (page === max_page) listen_right()
-        page--
-        picture.style.marginLeft = -((page - 1) * step_img) + 'rem'
-        pitch.style.marginLeft = -((page - 1) * step_pitch) + 'rem'
-
+        move(--page, 'down')
         bullets.item(page).classList.remove('active')
-        root.style.setProperty("--progress", ((page - 1) * 25) + "%")
         years.innerText--
-        if (page <= min_page) {
-            arrow_left.firstElementChild.style.opacity = "40%"
-            arrow_left.removeEventListener('click', down)
-        }
-        if (page < max_page) arrow_right.firstElementChild.style.opacity = "100%"
+        manageArrows()
     }
 
     function up(e) {
         e.preventDefault()
         if (page === min_page) listen_right()
-        picture.style.marginLeft = -((page) * step_img) + 'rem'
-        pitch.style.marginLeft = -((page) * step_pitch) + 'rem'
+        move(page, 'up')
         bullets.item(page).classList.add('active')
-        root.style.setProperty("--progress", ((page) * 25) + "%")
         page++
         years.innerText++
-        if (page >= max_page) {
-            arrow_right.firstElementChild.style.opacity = "40%"
-            arrow_right.removeEventListener('click', up)
-        }
-        if (page > min_page) arrow_left.firstElementChild.style.opacity = "100%"
+        manageArrows()
     }
 
+    function move(page, dir) {
+        dir === 'up' ? indice = 0 : indice = 1
+        picture.style.marginLeft = -((page - indice) * step_img) + 'rem'
+        pitch.style.marginLeft = -((page - indice) * step_pitch) + 'rem'
+        root.style.setProperty("--progress", ((page - indice) * 25) + "%")
+    }
+
+    function manageArrows() {
+        if (page > min_page) arrow_left.classList.add('active')
+        else {
+            arrow_left.classList.remove('active')
+            arrow_left.removeEventListener('click', down)
+        }
+
+        if (page < max_page) arrow_right.classList.add('active')
+        else {
+            arrow_right.classList.remove('active')
+            arrow_right.removeEventListener('click', up)
+        }
+
+    }
 })
